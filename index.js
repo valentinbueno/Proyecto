11e9d8bdf55a98ef56d66e24ed17b7c42feacd65
Felipejordan1 <fjordan@udesa.edu.ar>
2022-11-11 15:51:40 -0300

    avance 3 entrega 2

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-window.addEventListener("load,")
+let formulario = document.querySelector(".formulario")
 
-    let busqueda = document.querySelector(".search")
+let busqueda = document.querySelector(".search")
+let comentario =document.querySelector(".indicacion")
+let alerta = document.querySelector(".alerta")
 
-busqueda.addEventListener('sumbit',function()
 
-    if 
+
+formulario.addEventListener('submit',function(event){
+    event.preventDefault();
+    console.log(busqueda.value);
+    if (busqueda.value==""){
+       comentario.innerText = "No ingreso ningun dato"
+       alerta.style.display = "block"
+       busqueda.style.borderRadius = "10px 10px 0 0"
+    }
+
+    else if(busqueda.value.length<3){
+        comentario.innerText ="la busqueda debe tener al menos 3 carcteres"
+        alerta.style.display = "block"
+        busqueda.style.borderRadius = "10px 10px 0 0"
+    }
+
+    else{
+        this.sumbit()
+
+    }
+
+
+})
