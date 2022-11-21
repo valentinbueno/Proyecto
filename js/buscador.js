5c5eca7585f1a84197fc0ed19619118c7932ae3d
lucasbachrach <lbachrach@udesa.edu.ar>
2022-11-20 21:33:39 -0300

    avance buscador

--- js/buscador.js
+++ js/buscador.js
@@ -1,21 +1,21 @@
-let formulario = document.querySelector(".formulario")
-let busqueda = document.querySelector(".search")
-let comentario =document.querySelector(".indicacion")
-let alerta = document.querySelector(".alerta")
+//let formulario = document.querySelector(".formulario")
+//let busqueda = document.querySelector(".search")
+//let comentario =document.querySelector(".indicacion")
+//let alerta = document.querySelector(".alerta")
 
 
 
-let QueryString = location.search
+//let QueryString = location.search
 
 //construyo objeto literal
 
 
-let QSobject = new URLSearchParams(QueryString)
+//let QSobject = new URLSearchParams(QueryString)
 
 //obtengo el id de la propiedad del objeto literal
 
 
-let detail_id = QSobject.get('id')
+//let detail_id = QSobject.get('id')
 
 //endpoint con el id de la qs
 
@@ -26,64 +26,64 @@ let detail_id = QSobject.get('id')
 // location.search, se divide en dos partes ya que location es un objeto literal que nos sirve para solicitar informacion de la URL. y .search sirve para poder almacenar la querystring de una URL.
 //URLSearchParams sirve para definir distintos metodos utiles para poder trabajar con los parametros de una URL.
 
-formulario.addEventListener('submit',function(event){
-    event.preventDefault();
-    console.log(busqueda.value);
+//formulario.addEventListener('submit',function(event){
+    //event.preventDefault();
+    //console.log(busqueda.value);
     
-    if (busqueda.value==""){
-       comentario.innerText = "No ingreso ningun dato"
-       alerta.style.display = "block"
-       busqueda.style.borderRadius = "10px 10px 0 0"
-    }
+    //if (busqueda.value==""){
+       //comentario.innerText = "No ingreso ningun dato"
+       //alerta.style.display = "block"
+       //busqueda.style.borderRadius = "10px 10px 0 0"
+    //}
     
-    else if(busqueda.value.length<4){
-        comentario.innerText ="la busqueda debe tener al menos 3 caracteres"
-        alerta.style.display = "block"
-        busqueda.style.borderRadius = "10px 10px 0 0"
-    }
+    //else if(busqueda.value.length<4){
+        //comentario.innerText ="la busqueda debe tener al menos 3 caracteres"
+        //alerta.style.display = "block"
+        //busqueda.style.borderRadius = "10px 10px 0 0"
+    //}
 
 
 
-    else{
+    //else{
 
-    let url = `https://api.themoviedb.org/3/search/company?api_key=5af2599bc48eedc0c872d98ac992b8e3&query=${busqueda.value}&page=1`
-
-
-let resultado = documen.querySelector(".secciones")
+    //let url = `https://api.themoviedb.org/3/search/company?api_key=5af2599bc48eedc0c872d98ac992b8e3&query=${busqueda.value}&page=1`
 
 
-    fetch(url)
-         .then(function(response){
-         return response.json();
-         })
-        .then(function(data){
-         console.log(data);
+ //let resultado = documen.querySelector(".secciones")
 
 
-             if (data.result.length==0){
-                resultado.innerText = `No hay resultados para: ${busqueda.value}`
-            }
-            else{
-                titulo.innerText = `Resultado: ${busqueda.value}`
-            }
+     //fetch(url)
+          //.then(function(response){
+          //return response.json();
+          //})
+         //.then(function(data){
+          //console.log(data);
+
+
+              //if (data.result.length==0){
+                 //resultado.innerText = `No hay resultados para: ${busqueda.value}`
+             //}
+             //else{
+                 //titulo.innerText = `Resultado: ${busqueda.value}`
+             //}
         
-            for (i=0; i<6; i++){
-                `<h3 class="nombres z ingresado">${original_tittle}</h3>`
-                `<div class="full">`
-                    `<img class="peli" src=https://image.tmdb.org/t/p/w500${poster_path}>`
-                    `<h3 class="nombres ingresado">Nombre${tittle}</h3>`
-                `</div>`
-            }
+             //for (i=0; i<6; i++){
+                 //`<h3 class="nombres z ingresado">${original_tittle}</h3>`
+                 //`<div class="full">`
+                     //`<img class="peli" src=https://image.tmdb.org/t/p/w500${poster_path}>`
+                     //`<h3 class="nombres ingresado">Nombre${tittle}</h3>`
+                 //`</div>`
+             //}
 
 
-            })
+             //})
 
 
-            .catch(function(error){
-                console.log(error)
-            })
+             //.catch(function(error){
+                 //console.log(error)
+             //})
 
-        }})
+         //}})
 
 
 
@@ -122,3 +122,53 @@ let resultado = documen.querySelector(".secciones")
 // })
 
 //  })
+
+
+let formulario = document.querySelector(".formulario")
+let busqueda = document.querySelector(".busqueda")
+let comentario = document.querySelector(".indicacion")
+let alerta = document.querySelector(".alerta")
+
+let query_1 = location.search
+let query_2 = new URLSearchParams(query_1)
+let buscador_1 = query_2.get("buscador_1")
+
+let resultados = document.querySelector(".resultados")
+resultados.innerHTML += `Resultados para tu ${buscador_1}`
+
+let url = `https://api.themoviedb.org/3/search/company?api_key=5af2599bc48eedc0c872d98ac992b8e3&query=&page=1`
+
+fetch(url)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+        let resultadodebusqueda = document.querySelector(".resultados");
+        if (data.resultadodebusqueda.length==0){
+            resultadodebusqueda.innerText = `No hubo resultados para su ${buscador_1}`
+        } 
+        else{
+            for(i=0; i<6; i++);
+            let poster_path = data.resultados[i].poster_path
+            let imagen = `https://image.tmdb.org/t/p/w500/${poster_path}`
+            let id = data.resultados[i].id
+            container.innerHTML += `<article class = "peliculabuscada">
+                    <a href="./detalle-pelis.html?movie_id=${id}"> <img src="${imagen}" alt="Foto${data.results[i].original_title} " class="foto"></a>
+                    <p class = "titulo_de_la_pelicula">${data.results[i].original_title}</p>
+                    <p>${data.results[i].release_date}</p>
+                </article>`
+        }
+        //if(data.resultadodebusqueda==""){
+            //comentario.innerText = "No ingreso ningun dato"
+            //alerta.style.display = "block"
+            //busqueda.style.borderRadius = "10px 10px 0 0"
+        //}
+        //else if(data.resultadodebusqueda<4){
+            //comentario.innerText ="la busqueda debe tener al menos 3 caracteres"
+            //alerta.style.display = "block"
+            //busqueda.style.borderRadius = "10px 10px 0 0"
+
+        //}
+
+    })
